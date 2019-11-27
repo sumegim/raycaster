d8708966a844e66fa89ddc9ab74ebc8cf76a25e2
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2019-11-27 13:41:44 +0100

    Test the implemented function

---
+++ tests/test_implementation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from implementation import single_trilinear_interpolation
+
+def test_single_trilinear_interpolation():
+    # Vertices such that the front face of the cube is 1, the back face 0
+    vertices = np.array([[0,0,0,1],[1,0,0,1],[0,1,0,1],[1,1,0,1],[0,0,1,0],[1,0,1,0],[0,1,1,0],[1,1,1,0]])
+
+    # Point in the middle of the cube
+    assert single_trilinear_interpolation(np.array([0.5,0.5,0.5]), vertices) == 0.5
+    # Point in the middle of the front face
+    assert single_trilinear_interpolation(np.array([0.5,0.5,0]), vertices) == 1
+    # Point in the middle of the back face
+    assert single_trilinear_interpolation(np.array([0.5,0.5,1]), vertices) == 0
