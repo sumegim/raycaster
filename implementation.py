982e70ff79e0d4c81891671b35a83b3057077e53
Mark Sumegi <sumegi.mark@gmail.com>
2019-11-28 12:30:39 +0100

    naive compositing

--- implementation.py
+++ implementation.py
@@ -91,13 +91,12 @@ class RaycastRendererImplementation(RaycastRenderer):
                                      volume_center[2]
 
                 # Get voxel value
-                value = get_voxel(volume, voxel_coordinate_x, voxel_coordinate_y, voxel_coordinate_z)
-
+                c_i = self.tfunc.get_color(get_voxel(volume, voxel_coordinate_x, voxel_coordinate_y, voxel_coordinate_z))
                 # Normalize value to be between 0 and 1
-                red = value / volume_maximum
-                green = red
-                blue = red
-                alpha = 1.0 if red > 0 else 0.0
+                red = c_i.r
+                green = c_i.g
+                blue = c_i.b
+                alpha = 1.0
 
                 # Compute the color value (0...255)
                 red = math.floor(red * 255) if red < 255 else 255
@@ -156,27 +155,85 @@ class RaycastRendererImplementation(RaycastRenderer):
                     if (value > value_max):
                         value_max = value
 
-                    # Normalize value to be between 0 and 1
-                    red = value_max / volume_maximum
-                    green = red
-                    blue = red
-                    alpha = 1.0 if red > 0 else 0.0
+                # Normalize value to be between 0 and 1
+                red = value_max / volume_maximum
+                green = red
+                blue = red
+                alpha = 1.0 if red > 0 else 0.0
 
-                    # Compute the color value (0...255)
-                    red = math.floor(red * 255) if red < 255 else 255
-                    green = math.floor(green * 255) if green < 255 else 255
-                    blue = math.floor(blue * 255) if blue < 255 else 255
-                    alpha = math.floor(alpha * 255) if alpha < 255 else 255
+                # Compute the color value (0...255)
+                red = math.floor(red * 255) if red < 255 else 255
+                green = math.floor(green * 255) if green < 255 else 255
+                blue = math.floor(blue * 255) if blue < 255 else 255
+                alpha = math.floor(alpha * 255) if alpha < 255 else 255
 
-                    # Assign color to the pixel i, j
-                    image[(j * image_size + i) * 4] = red
-                    image[(j * image_size + i) * 4 + 1] = green
-                    image[(j * image_size + i) * 4 + 2] = blue
-                    image[(j * image_size + i) * 4 + 3] = alpha
+                # Assign color to the pixel i, j
+                image[(j * image_size + i) * 4] = red
+                image[(j * image_size + i) * 4 + 1] = green
+                image[(j * image_size + i) * 4 + 2] = blue
+                image[(j * image_size + i) * 4 + 3] = alpha
 
     # TODO: Implement Compositing function. TFColor is already imported. self.tfunc is the current transfer function.
     def render_compositing(self, view_matrix: np.ndarray, volume: Volume, image_size: int, image: np.ndarray):
-        pass
+        # Clear the image
+        self.clear_image()
+
+        # U vector. See documentation in parent's class
+        u_vector = view_matrix[0:3]
+
+        # V vector. See documentation in parent's class
+        v_vector = view_matrix[4:7]
+
+        # View vector. See documentation in parent's class
+        view_vector = view_matrix[8:11]
+
+        # Center of the image. Image is squared
+        image_center = image_size / 2
+
+        # Center of the volume (3-dimensional)
+        volume_center = [volume.dim_x / 2, volume.dim_y / 2, volume.dim_z / 2]
+        volume_maximum = volume.get_maximum()
+
+        # Define a step size to make the loop faster
+        step = 2 if self.interactive_mode else 1
+
+        for i in range(0, image_size, step):
+            for j in range(0, image_size, step):
+                c_prev = TFColor(0, 0, 0, 0)
+                for k in range(100, -100, -10):
+                    # Get the voxel coordinate X
+                    voxel_coordinate_x = u_vector[0] * (i - image_center) + v_vector[0] * (j - image_center) + \
+                                         volume_center[0] + k * view_vector[0]
+
+                    # Get the voxel coordinate Y
+                    voxel_coordinate_y = u_vector[1] * (i - image_center) + v_vector[1] * (j - image_center) + \
+                                         volume_center[1] + k * view_vector[1]
+
+                    # Get the voxel coordinate Z
+                    voxel_coordinate_z = u_vector[2] * (i - image_center) + v_vector[2] * (j - image_center) + \
+                                         volume_center[2] + k * view_vector[2]
+
+                    c_i = self.tfunc.get_color(get_voxel(volume, voxel_coordinate_x, voxel_coordinate_y, voxel_coordinate_z))
+                    # Get voxel value
+
+                    # # Normalize value to be between 0 and 1
+                    c_prev.r = c_i.r * c_i.a + (1 - c_i.a) * c_prev.r
+                    c_prev.g = c_i.g * c_i.a + (1 - c_i.a) * c_prev.g
+                    c_prev.b = c_i.b * c_i.a + (1 - c_i.a) * c_prev.b
+
+
+                # Compute the color value (0...255)
+                red = math.floor(c_prev.r * 255) if c_prev.r < 255 else 255
+                green = math.floor(c_prev.g * 255) if c_prev.g < 255 else 255
+                blue = math.floor(c_prev.b * 255) if c_prev.b < 255 else 255
+                alpha = 255
+
+                # Assign color to the pixel i, j
+                image[(j * image_size + i) * 4] = red
+                image[(j * image_size + i) * 4 + 1] = green
+                image[(j * image_size + i) * 4 + 2] = blue
+                image[(j * image_size + i) * 4 + 3] = alpha
+
 
     # TODO: Implement function to render multiple energy volumes and annotation volume as a silhouette.
     def render_mouse_brain(self, view_matrix: np.ndarray, annotation_volume: Volume, energy_volumes: dict,
